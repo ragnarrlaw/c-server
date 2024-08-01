0b9b508bf4fac18fa95ab4e0964de5cc683f048d
BhathiyaVandebona <malingabhathiya@gmail.com>
2024-08-01 21:06:26 +0530

    add mime type detection functionality to the file operations

--- mime/mime.c
+++ mime/mime.c
@@ -1,1 +1,23 @@
 #include "mime.h"
+#include <stdlib.h>
+#include <string.h>
+
+const char *get_file_mime_type(const char *buffer, size_t buffer_length) {
+  if (buffer_length >= MAGIC_SIZE && memcmp(buffer, "\xFF\xD8\xFF", 3) == 0) {
+    return "image/jpeg";
+  }
+  if (buffer_length >= MAGIC_SIZE &&
+      memcmp(buffer, "\x89PNG\r\n\x1A\n", 8) == 0) {
+    return "image/png";
+  }
+  if (buffer_length >= MAGIC_SIZE && memcmp(buffer, "%PDF-", 5) == 0) {
+    return "application/pdf";
+  }
+  if (buffer_length >= 5 && memcmp(buffer, "{\n", 2) == 0) {
+    return "application/json";
+  }
+  if (buffer_length >= 4 && memcmp(buffer, "<!DOCTYPE", 9) == 0) {
+    return "text/html";
+  }
+  return "application/octet-stream";
+}
--- mime/mime.h
+++ mime/mime.h
@@ -1,4 +1,9 @@
 #ifndef _MIME_H
 #define _MIME_H
 
+#include <stdlib.h>
+#define MAGIC_SIZE 8
+
+const char *get_file_mime_type(const char* buffer, size_t len);
+
 #endif /* ifndef _MIME_H */
