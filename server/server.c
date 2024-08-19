31e1ac2c413a5d221de07957a6e8ffc7020d1809
BhathiyaVandebona <malingabhathiya@gmail.com>
2024-08-19 23:42:12 +0530

    add code for initializing server

--- server/server.c
+++ server/server.c
@@ -1,2 +1,109 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "../request/request.h"
 #include "server.h"
 
+#define PORT "8080"
+#define BACKLOG 10
+
+void *get_in_addr(struct sockaddr *);
+void *handle_client(void *);
+
+void run_server() {
+
+  int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr; // connector's address information
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes = 1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    exit(EXIT_FAILURE);
+  }
+
+  for (p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+
+    break;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if (p == NULL) {
+    fprintf(stderr, "server: failed to bind\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+
+  printf(">>>> Server: listening...\n");
+
+  while (1) {
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_fd == -1) {
+      perror("accept");
+      continue;
+    }
+
+    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),
+              s, sizeof s);
+    printf(">>>> Server: client connection from %s\n", s);
+
+    // implement the handling function using pthread
+    pthread_t thread;
+  }
+}
+
+// get sockaddr, IPv4 or IPv6
+void *get_in_addr(struct sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in *)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+void *handle_client(void *data) {
+  printf(">>>> Handling client request...\n");
+
+  return NULL;
+}
--- server/server.h
+++ server/server.h
@@ -1,6 +1,7 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
-
+void run_server();
+void stop_server();
 
 #endif /* ifndef _SERVER_H */
